05bd2141e7a2038434a1c808bb1f17b374dc3256
Anel Brcaninovic <brcaninovicanel2@gmail.com>
2024-03-18 18:10:34 +0100

    GoogleOauth Button

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Container} from "@material-ui/core";
 
 import { BrowserRouter,Switch, Route } from 'react-router-dom';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import Navbar from './components/navbar/navbar';
 import Home from './components/home/home';
@@ -13,15 +13,17 @@ import Auth from './components/auth/auth';
 const App = () =>{
 
     return(
-        <BrowserRouter>
-            <Container maxwidth="lg">
-                <Navbar />
-                <Switch>
-                    <Route path="/auth" exact component={Auth} />
-                    <Route path="/" exact component={Home} />
-                </Switch>
-            </Container>
-        </BrowserRouter>
+        <GoogleOAuthProvider clientId="316952771777-2mo4duu8rnavgt6h6sgh2ot7ubahb3mi.apps.googleusercontent.com">
+            <BrowserRouter>
+                <Container maxwidth="lg">
+                    <Navbar />
+                    <Switch>
+                        <Route path="/auth" exact component={Auth} />
+                        <Route path="/" exact component={Home} />
+                    </Switch>
+                </Container>
+            </BrowserRouter>
+        </GoogleOAuthProvider>
     );
 }
 export default App;
--- client/src/components/auth/auth.js
+++ client/src/components/auth/auth.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useState } from 'react';
 import {Grow, Avatar,Paper,Grid,Typography,Container, Button } from '@material-ui/core';
-import  LockOutlinedIcon from "@material-ui/icons/LockOutlined"
-import useStyles from "./styles"
+import {GoogleLogin } from '@react-oauth/google';
+import  LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import useStyles from "./styles";
 import Input from './input';
-
 
 const Auth = () => {
     const classes = useStyles();
@@ -15,6 +15,14 @@ const Auth = () => {
     const handleShowPassword = () =>setShowPassword((prevShowPassword) => !prevShowPassword);
     const switchMode = () => {setIsSignup((prevSignUp) => !prevSignUp); setShowPassword(false);};
 
+    const googleSuccess = async (res) =>{
+        console.log(res);
+    };
+    const googleFailure = (error) =>{
+        console.log(error);
+        console.log("Google Sign in was unsuccessful!");
+    };
+    
   return (
     <Grow in>
 
@@ -37,9 +45,16 @@ const Auth = () => {
                         <Input name="password" label="Password"  handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword}/>
                         {isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
                 </Grid>
+
                 <Button type='submit' fullWidth variant="contained" color='primary' className={classes.submit}>
                     {isSignup ? 'Sign up' : 'Sign in'}
                 </Button>
+
+                    <GoogleLogin 
+                       
+                        onSuccess={googleSuccess}
+                        onFailure={googleFailure}
+                    />
 
                 <Grid container justify-content="flex-end">
                         <Grid item>
